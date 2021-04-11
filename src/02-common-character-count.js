de37fa5bf5d7b77b63243eaf54664f1c1a18624a
SK-website <vnali@tutanota.com>
2021-04-11 15:18:04 +0300

    solution

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,26 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const arrStr1 = s1.split('');
+  const arrStr2 = s2.split('');
+  let res = 0;
+  const found = [];
+
+  for (let i = 0; i < arrStr1.length; i++) {
+    const isFound = Boolean(found.find((el) => arrStr1[i] === el));
+    if (!isFound) {
+      const n1 = arrStr1.filter((el) => arrStr1[i] === el);
+      const n2 = arrStr2.filter((el) => arrStr1[i] === el);
+      const minNumber = n1.length > n2.length ? n2.length : n1.length;
+
+      if (minNumber) {
+        res += minNumber;
+        found.push(arrStr1[i]);
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = getCommonCharacterCount;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,19 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const indexes = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) indexes.push(i);
+  }
+  const filteredArr = arr.filter((el) => el > -1);
+  const sortedArr = filteredArr.sort((prev, next) => prev - next);
+
+  for (let j = 0; j < indexes.length; j++) {
+    sortedArr.splice(indexes[j], 0, -1);
+  }
+  return sortedArr;
 }
 
 module.exports = sortByHeight;
