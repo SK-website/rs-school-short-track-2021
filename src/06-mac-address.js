f80ef7be46c5e511e93df3187c36b23e506683b7
SK-website <vnali@tutanota.com>
2021-04-11 22:53:48 +0300

    solution 06

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,10 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const regExp1 = /^[0-9A-F]{2}([-][0-9A-F]{2}){5}$/;
+  const res = regExp1.test(n);
+  return res;
 }
 
 module.exports = isMAC48Address;
