5c252714ccc808fea28567096e43bc309b6a1475
SK-website <vnali@tutanota.com>
2021-04-12 01:55:38 +0300

    solution 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,32 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let number;
+  const res = [];
+  const arrString = str.split('');
+
+  function repeatNumber(item, i) {
+    const j = i + 1;
+    if (arrString[j] !== item) return;
+
+    number += 1;
+    repeatNumber(arrString[j], j);
+  }
+
+  for (let i = 0; i < arrString.length;) {
+    number = 1;
+    repeatNumber(arrString[i], i);
+    if (number > 1) {
+      res.push(`${number}${arrString[i]}`);
+      i += number;
+    } else {
+      res.push(arrString[i]);
+      i += 1;
+    }
+  }
+
+  return res.join('');
 }
 
 module.exports = encodeLine;
